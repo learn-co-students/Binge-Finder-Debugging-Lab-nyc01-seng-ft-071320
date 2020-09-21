547e7a3d1ed458f385680d780610f9c5b17e9711
brianwlego <legobw@gmail.com>
2020-09-20 22:12:00 -0400

    Done.

--- src/Components/App.js
+++ src/Components/App.js
@@ -14,30 +14,52 @@ class App extends Component {
     selectedShow: "",
     episodes: [],
     filterByRating: "",
+    pageNum: 1
   }
 
   componentDidMount = () => {
-    Adapter.getShows().then(shows => this.setState({shows}))
+    Adapter.getShows(this.state.pageNum).then(shows => this.setState({shows}))
+    // window.addEventListener('scroll', this.handleScroll, true);
   }
 
   componentDidUpdate = () => {
     window.scrollTo(0, 0)
   }
 
-  handleSearch (e){
+  // componentWillUnmount(){
+  //   window.removeEventListener('scroll', this.handleScroll);
+  // }
+
+  handleSearch = (e) => {
     this.setState({ searchTerm: e.target.value.toLowerCase() })
   }
 
   handleFilter = (e) => {
-    e.target.value === "No Filter" ? this.setState({ filterRating:"" }) : this.setState({ filterRating: e.target.value})
+    e.target.value === "No Filter" ? this.setState({ filterByRating:"" }) : this.setState({ filterByRating: e.target.value})
   }
+
+  // handleScroll = (e) => {
+  //   let el = e.target.scrollingElement
+  //   if (el.scrollHeight - el.scrollTop === el.clientHeight) {
+      
+  //     Adapter.getShows(this.state.pageNum + 1)
+  //     .then(shows => {
+  //       this.setState(()=>({
+  //         shows: this.state.shows.concat(shows)
+  //       }), console.log("after fetch", this.state.shows))
+  //     })
+  //     window.scrollTo(0, 0)
+  //   }
+  // }
 
   selectShow = (show) => {
     Adapter.getShowEpisodes(show.id)
-    .then((episodes) => this.setState({
-      selectedShow: show,
-      episodes
-    }))
+    .then(episodesResp => {
+      this.setState(() => ({
+        selectedShow: show,
+        episodes: episodesResp
+      }))
+    })
   }
 
   displayShows = () => {
@@ -52,14 +74,14 @@ class App extends Component {
 
   render (){
     return (
-      <div>
+      <div >
         <Nav handleFilter={this.handleFilter} handleSearch={this.handleSearch} searchTerm={this.state.searchTerm}/>
         <Grid celled>
           <Grid.Column width={5}>
             {!!this.state.selectedShow ? <SelectedShowContainer selectedShow={this.state.selectedShow} allEpisodes={this.state.episodes}/> : <div/>}
           </Grid.Column>
           <Grid.Column width={11}>
-            <TVShowList shows={this.displayShows()} selectShow={this.selectShow} searchTerm={this.state.searchTerm}/>
+            <TVShowList shows={this.displayShows()} selectShow={this.selectShow} searchTerm={this.state.searchTerm} />
           </Grid.Column>
         </Grid>
       </div>
