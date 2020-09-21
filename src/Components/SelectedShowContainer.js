df9e3944987d9ec7a7fc2c2045f1a2c3caa48f51
junko911 <junkotahara911@gmail.com>
2020-09-21 09:46:31 -0400

    Add display sshow detail/search by name/filter by rating features

--- src/Components/SelectedShowContainer.js
+++ src/Components/SelectedShowContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Episode from './Components/Episode';
+import Episode from './Episode';
 
 class SelectedShowContainer extends Component {
 
@@ -9,7 +9,7 @@ class SelectedShowContainer extends Component {
 
   mapSeasons = () => {
     if (!!this.props.episodes){
-      let seasons = this.props.episodes.map((e)=> e.season).unique()
+      let seasons = this.props.allEpisodes.map((e)=> e.season).unique()
 
       return seasons.map((s) => {
         return (<option value={s} key={s}>Season {s}</option>)
@@ -18,9 +18,11 @@ class SelectedShowContainer extends Component {
   }
 
   mapEpisodes = () => {
-    return this.props.episodes.map((e)=>{
-      if (e.season == this.state.selectedSeason){
+    return this.props.allEpisodes.map((e)=>{
+      if (e.season === this.state.selectedSeason){
         return (<Episode eachEpisode={e} key={e.id}/>)
+      } else {
+        return null
       }
     })
   }
@@ -51,9 +53,9 @@ class SelectedShowContainer extends Component {
 
 }
 
-export SelectedShowContainer;
+export default SelectedShowContainer;
 
-
+// eslint-disable-next-line 
 Array.prototype.unique = function() {
   var arr = [];
   for(var i = 0; i < this.length; i++) {
