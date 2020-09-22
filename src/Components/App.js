b8ae39216dc1402eb256131f50b006ebcfd813b3
irosen419 <irosen419@gmail.com>
2020-09-21 20:50:28 -0400

    Done.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Adapter from '../Adapter';
 import TVShowList from './TVShowList';
 import Nav from './Nav';
 import SelectedShowContainer from './SelectedShowContainer';
@@ -17,32 +16,43 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    Adapter.getShows().then(shows => this.setState({shows}))
+    this.getShows().then(shows => this.setState({ shows }))
+    // console.log(Adapter)
+  }
+
+  getShows() {
+    return fetch("http://api.tvmaze.com/shows")
+      .then(res => res.json())
+  }
+
+  getShowEpisodes(showID) {
+    return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`)
+      .then(res => res.json)
   }
 
   componentDidUpdate = () => {
     window.scrollTo(0, 0)
   }
 
-  handleSearch (e){
+  handleSearch = (e) => {
     this.setState({ searchTerm: e.target.value.toLowerCase() })
   }
 
   handleFilter = (e) => {
-    e.target.value === "No Filter" ? this.setState({ filterRating:"" }) : this.setState({ filterRating: e.target.value})
+    e.target.value === "No Filter" ? this.setState({ filterByRating: "" }) : this.setState({ filterByRating: e.target.value })
   }
 
   selectShow = (show) => {
-    Adapter.getShowEpisodes(show.id)
-    .then((episodes) => this.setState({
-      selectedShow: show,
-      episodes
-    }))
+    this.getShowEpisodes(show.id)
+      .then((episodes) => this.setState({
+        selectedShow: show,
+        episodes: [...this.state.episodes, episodes]
+      }))
   }
 
   displayShows = () => {
-    if (this.state.filterByRating){
-      return this.state.shows.filter((s)=> {
+    if (this.state.filterByRating) {
+      return this.state.shows.filter((s) => {
         return s.rating.average >= this.state.filterByRating
       })
     } else {
@@ -50,16 +60,16 @@ class App extends Component {
     }
   }
 
-  render (){
+  render() {
     return (
       <div>
-        <Nav handleFilter={this.handleFilter} handleSearch={this.handleSearch} searchTerm={this.state.searchTerm}/>
+        <Nav handleFilter={this.handleFilter} handleSearch={this.handleSearch} searchTerm={this.state.searchTerm} />
         <Grid celled>
           <Grid.Column width={5}>
-            {!!this.state.selectedShow ? <SelectedShowContainer selectedShow={this.state.selectedShow} allEpisodes={this.state.episodes}/> : <div/>}
+            {!!this.state.selectedShow ? <SelectedShowContainer selectedShow={this.state.selectedShow} episodes={this.state.episodes} /> : <div />}
           </Grid.Column>
           <Grid.Column width={11}>
-            <TVShowList shows={this.displayShows()} selectShow={this.selectShow} searchTerm={this.state.searchTerm}/>
+            <TVShowList shows={this.displayShows()} selectShow={this.selectShow} searchTerm={this.state.searchTerm} />
           </Grid.Column>
         </Grid>
       </div>
